12658d56a9d2dd2853ce6dfae15669b624faa6a1
ccsnd <ccs@nd.edu>
2024-10-26 11:17:21 -0400

    forgot time zone

--- my-app/src/Events.js
+++ my-app/src/Events.js
@@ -8,7 +8,7 @@ const Events = () => {
         </h2>
         
         {/* Embed Google Calendar */}
-        <iframe src="https://calendar.google.com/calendar/embed?src=c_2398dd66f60145fcc886b91b83917220a09921721a5f86de7992dd5873b62a07%40group.calendar.google.com&ctz=America%2FLos_Angeles" 
+        <iframe src="https://calendar.google.com/calendar/embed?src=c_2398dd66f60145fcc886b91b83917220a09921721a5f86de7992dd5873b62a07%40group.calendar.google.com&ctz=America/New_York" 
         style={{ border: '0', width: '800px', height: '600px', overflow: 'auto' }} 
         
         title="Google Calendar">
