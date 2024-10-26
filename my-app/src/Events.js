2bc86a68be7be12f57ceef07488392f3309c126a
ccsnd <ccs@nd.edu>
2024-10-26 00:06:37 -0400

    hurrhh

--- my-app/src/Events.js
+++ my-app/src/Events.js
@@ -6,7 +6,7 @@ const Events = () => {
         <h2>
             Maybe we can have an embedded google calendar api but as of now...
         </h2>
-        <img src="public/ron.jpg" alt="A cute cat" width="300" height="200"></img>
+        <img src="/ron.jpg" alt="A cute cat" width="300" height="200"></img>
     </div>
 
   );
