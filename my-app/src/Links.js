cd21eee6ea0200f9d814948c51ebc242decf9290
ccsnd <ccs@nd.edu>
2024-10-26 10:44:34 -0400

    FIX refresh?

--- my-app/src/Links.js
+++ my-app/src/Links.js
@@ -2,9 +2,9 @@ import React from 'react';
 
 const Links = () => {
   return (
-    <div>
+    <div className="text-center">
       <h2>Home Page</h2>
-      <p>Welcome to the home page! Here are some useful links:</p>
+      <p >Welcome to the home page! Here are some useful links:</p>
       <ul>
         <li><a href="https://shop.nd.edu/C21688_ustores/web/product_detail.jsp?PRODUCTID=12029&SINGLESTORE=true" target="_blank" rel="noopener noreferrer">PAY DUES HERE</a></li>
         <li><a href="https://calendar.google.com/calendar/u/0?cid=Y18yMzk4ZGQ2NmY2MDE0NWZjYzg4NmI5MWI4MzkxNzIyMGEwOTkyMTcyMWE1Zjg2ZGU3OTkyZGQ1ODczYjYyYTA3QGdyb3VwLmNhbGVuZGFyLmdvb2dsZS5jb20" target="_blank" rel="noopener noreferrer">GET EVENT CALENDAR HERE</a></li>
