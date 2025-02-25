9365c0587c13f039a63a3fa76441ecccf22deeab
Aaron Wang <aaronwang04@icloud.com>
2025-02-24 20:11:02 -0500

    calendar

--- my-app/src/Events.js
+++ my-app/src/Events.js
@@ -8,12 +8,8 @@ const Events = () => {
       </h2>
       <div className="calendar-container">
         {/* Embed Google Calendar */}
-        <iframe src="https://calendar.google.com/calendar/embed?src=c_2398dd66f60145fcc886b91b83917220a09921721a5f86de7992dd5873b62a07%40group.calendar.google.com&ctz=America/New_York"
-          style={{ border: '0', overflow: 'auto', maxWidth: '800px' }}
-
+        <iframe src="https://calendar.google.com/calendar/embed?src=c_2398dd66f60145fcc886b91b83917220a09921721a5f86de7992dd5873b62a07%40group.calendar.google.com&ctz=America/New_York&mode=AGENDA"
           title="Google Calendar">
-
-
         </iframe>
       </div>
     </div>
